c02367818749dea7ec5340f74d44856df2c1beff
Mali Michael <malimccalla@gmail.com>
2017-07-16 16:37:54 +0100

    Date input selection

--- src/request.es6.jsx
+++ src/request.es6.jsx
@@ -7,35 +7,49 @@ class Request extends Component {
     const API_KEY = process.env.REACT_APP_API_KEY;
     const URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=";
 
-      this.state = {
+    this.state = {
       url: URL + API_KEY,
-      response: null
+      response: null,
+      date: null
     };
   }
 
-  componentWillMount() {
-  this.getData();
-  }
-
   getData () {
-    fetch(this.state.url).then(r => r.json())
+    fetch(this.state.url).then(res => res.json())
     .then((data) => {this.updateState(data);})
     .catch(e => console.log("Error"));
   }
 
+  handleDateChange(date) {
+    const dateString = date.target.value.toString();
+    this.setState({ date: dateString });
+  }
+
+  handleButtonClick() {
+    this.getData();
+  }
+
   updateState(data) {
-  this.setState({
-    response: data
-  });
-}
+    this.setState({
+      response: data
+    });
+  }
 
 
   render() {
     if (this.state.response) {
-      return (<p className="request">Microsoft latest share price : {this.state.response["Time Series (Daily)"]["2017-07-14"]["4. close"]}</p>);
+      console.log(this.state.response);
+      return (
+        <p className="request">
+          Microsoft  share price : {this.state.response["Time Series (Daily)"][this.state.date]["4. close"]}
+        </p>
+      );
     } else {
       return (
-        <p></p>
+        <div>
+          <input type="date" onChange={this.handleDateChange.bind(this)}/>
+          <button onClick={this.handleButtonClick.bind(this)}>Search</button>
+        </div>
       );
     }
   }
